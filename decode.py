96764870c1c8591d984e3927251892aec60b498d
Tomo <94550718+Tomo1151@users.noreply.github.com>
2022-09-13 05:35:09 +0900

    add decoder

--- decode.py
+++ decode.py
@@ -3,8 +3,9 @@ import struct
 import time
 import png
 
+
 def calc_sheared_map(width, height, rg, rb, gb, rgb_data):
-    start = time.time()
+    # start = time.time()
     result = [None] * width * height
     RED   = 0
     GREEN = 1
@@ -41,31 +42,170 @@ def calc_sheared_map(width, height, rg, rb, gb, rgb_data):
         result[idx] = pixel
         idx += 1
 
-    end = time.time()
+    # end = time.time()
 
-    print(f"calc shear map time: {end - start} sec")
+    # print(f"calc shear map time: {end - start} sec")
     return result
 
-start_time = time.time();
+def PaethPredictor(a, b, c):
+    p = a + b - c
+    pa = abs(p - a)
+    pb = abs(p - b)
+    pc = abs(p - c)
+    if pa <= pb and pa <= pc:
+        Pr = a
+    elif pb <= pc:
+        Pr = b
+    else:
+        Pr = c
+    return Pr
 
-reader = png.Reader(filename='output/deflate.png')
-width, height, pixels, metadata = reader.read_flat()
-
-# greyscale = metadata['greyscale']
-# alpha = metadata['alpha']
-# planes = metadata['planes']
-# bitdepth = metadata['bitdepth']
-# pixel_width = 4 if alpha else 3
+# start_time = time.time();
 
 
-pixel_count = len(pixels)
+
+### Reading the image
+image = png.Reader('output/deflate.png')
+
+### Def chunks
+IHDR = None
+PLTE = None
+IDAT = None
+tRNS = None
+gAMA = None
+cHRM = None
+sRGB = None
+iCCP = None
+tEXt = None
+zTXt = None
+iTXt = None
+bKGD = None
+pHYs = None
+sBIT = None
+sPLT = None
+hIST = None
+tIME = None
+icPT = None
+IEND = None
+
+### Def Compression method
+Deflate = 0
+LZMA    = 1
+LZHAM   = 2
+
+### Analyze the image chunks
+for chunk in image.chunks():
+    chunk_type = chunk[0]
+    chunk_data = chunk[1]
+
+    if chunk_type == b'IHDR':
+        IHDR = chunk_data
+    if chunk_type == b'PLTE':
+        PLTE = chunk_data
+    if chunk_type == b'IDAT':
+        IDAT = chunk_data
+    if chunk_type == b'tRNS':
+        tRNS = chunk_data
+    if chunk_type == b'gAMA':
+        gAMA = chunk_data
+    if chunk_type == b'cHRM':
+        cHRM = chunk_data
+    if chunk_type == b'sRGB':
+        sRGB = chunk_data
+    if chunk_type == b'iCCP':
+        iCCP = chunk_data
+    if chunk_type == b'tEXt':
+        tEXt = chunk_data
+    if chunk_type == b'zTXt':
+        zTXt = chunk_data
+    if chunk_type == b'iTXt':
+        iTXt = chunk_data
+    if chunk_type == b'bKGD':
+        bKGD = chunk_data
+    if chunk_type == b'pHYs':
+        pHYs = chunk_data
+    if chunk_type == b'sBIT':
+        sBIT = chunk_data
+    if chunk_type == b'sPLT':
+        sPLT = chunk_data
+    if chunk_type == b'hIST':
+        hIST = chunk_data
+    if chunk_type == b'tIME':
+        tIME = chunk_data
+    if chunk_type == b'icPT':
+        icPT = chunk_data
+    if chunk_type == b'IEND':
+        IEND = chunk_data
+
+### Reading IHDR
+width, height, bit_depth, color_type, comp_method, filter_method, interlace = struct.unpack('>IIBBBBB', IHDR)
+
+
+### Decompress IDAT
+if color_type == 4 or color_type == 6:
+    bytes_per_pixel = 4
+elif color_type == 2:
+    bytes_per_pixel = 3
+
+Recon = []
+stride = width * bytes_per_pixel
+
+previous = None
+filtered_RGB_data = None
+IDAT_length = len(IDAT)
+scanline_length = 3 * width
+
+if comp_method == Deflate:
+    filtered_RGB_data = zlib.decompress(IDAT)
+elif comp_method == LZMA:
+    filtered_RGB_data = lzma.decompress(IDAT)
+elif comp_method == LZHAM:
+    filtered_RGB_data = lzham.decompress(IDAT, IDAT_length)
+
+
+def Recon_a(r, c):
+    return Recon[r * stride + c - bytes_per_pixel] if c >= bytes_per_pixel else 0
+
+def Recon_b(r, c):
+    return Recon[(r-1) * stride + c] if r > 0 else 0
+
+def Recon_c(r, c):
+    return Recon[(r-1) * stride + c - bytes_per_pixel] if r > 0 and c >= bytes_per_pixel else 0
+
+idx = 0
+for r in range(height):
+    filter_type = filtered_RGB_data[idx]
+    idx += 1
+
+    for c in range(3 * width):
+        Filt_x = filtered_RGB_data[idx]
+        idx += 1
+
+        if filter_type == 0:
+            Recon_x = Filt_x
+        elif filter_type == 1:
+            Recon_x = Filt_x + Recon_a(r, c)
+        elif filter_type == 2:
+            Recon_x = Filt_x + Recon_b(r, c)
+        elif filter_type == 3:
+            Recon_x = Filt_x + (Recon_a(r, c) + Recon_b(r, c)) // 2
+        elif filter_type == 4:
+            Recon_x = Filt_x + PaethPredictor(Recon_a(r, c), Recon_b(r, c), Recon_c(r, c))
+        else:
+            print(filter_type)
+
+        Recon.append(Recon_x & 0xff)
+        # RGB_Map[idx] = (Recon_x & 0xff)
+
+
+pixel_count = width * height
 start = 0
 split = 3
 idx = 0
 rgb_data = [None] * width * height
 
-for i in pixels:
-    rgb_data[idx] = pixels[start:start+split:1]
+for i in Recon:
+    rgb_data[idx] = Recon[start:start+split:1]
     start += split
     idx += 1
     if start >= pixel_count:
@@ -74,15 +214,15 @@ for i in pixels:
 # print(w)
 # print(h)
 # print(metadata)
-# print(type(rgb_data[0][0]))
+print(rgb_data[0][0:12])
 
-image_array = calc_sheared_map(width, height, 69, 88, 23, rgb_data)
+# image_array = calc_sheared_map(width, height, 69, 88, 23, rgb_data)
 
-print(image_array[0:3])
+# print(image_array[0:3])
 
-end_time = time.time()
+# end_time = time.time()
 
-print(f"decode time: {end_time - start_time} sec")
+# print(f"decode time: {end_time - start_time} sec")
 
 
 
